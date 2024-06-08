97ef5bf51edcbf591b95d01347e80d3f8ba58f07
Lingbo <ilovemc@zohomail.cn>
2024-06-08 19:56:11 +0800

    v1.4.0 Add caching feature, improve documents, detail changes and optimizations (#19)
    * v1.4.0 Add caching feature, improve documents, detail changes and optimizations
    
    * 紧急修复
    
    ---------
    
    Authored-by: lingbopro <ilovemc@zohomail.cn>
    Co-authored-by: Minemetero <156386015+Minemetero@users.noreply.github.com>

---
+++ docs/dev-guide.zh-CN.md
@@ -0,0 +1,97 @@
+# Easy GitHub Hosts - 开发指南
+
+本文档为希望理解、修改或为 Easy GitHub Hosts 项目做出贡献的开发者提供了一份详细的指南。  
+
+## 目录
+
+- [Easy GitHub Hosts - 开发指南](#easy-github-hosts---开发指南)
+  - [目录](#目录)
+  - [项目结构](#项目结构)
+  - [代码概述](#代码概述)
+    - [ipFetcher.js](#ipfetcherjs)
+    - [updateHosts.js](#updatehostsjs)
+    - [restoreHosts.js](#restorehostsjs)
+    - [main.js](#mainjs)
+  - [贡献](#贡献)
+
+## 项目结构
+
+该项目的结构如下：
+
+easy-github-hosts/  
+├──.github/  
+│   ├── ISSUE_TEMPLATE/  
+│   ├── bug_report.md  
+│   └── feature_request.md  
+├── docs/  
+│   ├── CONTRIBUTING.en-US.md  
+│   ├── dev-guide.en-US.md    
+│   └── dev-guide.zh-CN.md  
+├── files/  
+│   ├── backup/  
+│   └── cache/  
+├── ipFetcher.js  
+├── LICENSE  
+├── main.js  
+├── package.json  
+├── package-lock.json  
+├── README.md  
+├── restoreHosts.js  
+└── updateHosts.js  
+
+- `.github/`: 与 GitHub 相关的文件。  
+  - `ISSUE_TEMPLATE/`: 包含问题模板文件。  
+    - `bug_report.md`: 是的！用于报告 bug 的问题。  
+    - `feature_request.md`: 是的！用于功能请求的问题。  
+- `docs/`: 包含文档文件。  
+  <!-- - `README.md`: 提供通用信息和使用说明。 -->
+  - `dev-guide.en-US.md`: 开发指南（英文）。  
+  - `dev-guide.zh-CN.md`: 本开发指南（中文）。  
+- `files/`: 包含此程序生成的文件。  
+  - `backup/`: 包含 HOSTS 文件的备份。  
+  - `cache/`: 包含 IP 缓存。  
+- `ipFetcher.js`: 包含从 GitHub 相关域获取 IP 地址的函数。  
+- `main.js`: 该程序的主入口点。  
+- `package.json`: 管理项目依赖项和脚本。  
+- `restoreHosts.js`: 包含还原原始 `hosts` 文件的函数。  
+- `updateHosts.js`: 包含使用新 IP 地址更新 `hosts` 文件的函数。  
+
+## 代码概述
+
+### ipFetcher.js
+
+此文件包含从 fetch 获取 GitHub 相关域的 IP 地址的逻辑：  
+
+`getIP(host)`: 获取给定主机的 IP 地址。  
+`getIPs(cache)`: 获取所有预定义的 GitHub 相关域的 IP 地址。如果 `cache` 参数为 true，它还将缓存所有 IP 地址。  
+`cacheIPs(IPs)`: 将给定的 IP 地址缓存到文件中。  
+`readCache()`: 读取缓存的 IP 地址。  
+
+### updateHosts.js
+此文件包含更新 hosts 文件的逻辑：  
+
+`checkIPv4(IP)`: 检查字符串是否为有效的 IPv4 地址。  
+`parseHostsRecord(record)`: 解析 hosts 文件中的单个记录。  
+`createBackup(hostsPath)`: 创建 hosts 文件的备份。  
+`updateHosts()`: 更新 hosts 文件的主函数。  
+`getLines(content)`: 获取 `content` 的行。  
+`findByItemProperty(array, property, find)`: 在 `array` 中查找第一个属性 `property` 等于 `find` 的项。  
+
+### restoreHosts.js
+此文件包含从备份还原原始 hosts 文件的逻辑：  
+
+`restoreHosts()`: 还原 hosts 文件的主函数，从备份中还原。  
+
+### main.js
+此文件充当程序的入口点。它导入并运行 updateHosts.js 中的 updateHosts 函数。  
+
+## 贡献
+欢迎您的贡献！请遵循以下指南：
+
+Fork 存储库。  
+为您的功能或 bug 修复创建一个新分支。  
+为您的更改编写代码。  
+使用 [Prettier](https://prettier.cn) 格式化您的代码。  
+使用描述性消息提交您的更改。  
+将您的分支推送到您的 fork。  
+向主存储库提交拉取请求 **(dev 分支)**。  
--- ipFetcher.js
+++ ipFetcher.js
@@ -2,41 +2,25 @@
 
 "use strict";
 
-const axios = require("axios");
-const cheerio = require("cheerio");
+const fs = require("fs");
+const path = require("path");
 
 const appName = "Easy GitHub Hosts";
 const sites = [
-    "actions.github.com",
-    "actions.githubusercontent.com",
-    "api.github.com",
-    "apps.codespaces.githubusercontent.com",
-    "assets-cdn.githubusercontent.com",
-    "assets.github.com",
-    "avatar.githubusercontent.com",
-    "avatars0.githubusercontent.com",
-    "avatars1.githubusercontent.com",
-    "avatars2.githubusercontent.com",
-    "avatars3.githubusercontent.com",
-    "avatars4.githubusercontent.com",
-    "avatars5.githubusercontent.com",
-    "avatars6.githubusercontent.com",
-    "avatars7.githubusercontent.com",
-    "avatars8.githubusercontent.com",
-    "camo.githubusercontent.com",
-    "copilot.github.com",
-    "desktop.github.com",
-    "developer.github.com",
-    "docs.github.com",
+    // 理论上只需要包含二级域名，子域名的IP会从二级域名查询
+    // 但是为什么githubusercontent.com查不到呢...
+    "avatars.githubusercontent.com",
     "ghcr.io",
-    "gist.github.com",
     "gist.githubusercontent.com",
     "github.blog",
     "github.com",
     "github.community",
     "github.dev",
+    "github.githubassets.com",
+    "github.io",
+    "githubassets.com",
     "githubstatus.com",
-    "help.github.com",
+    "githubusercontent.com",
     "images.githubusercontent.com",
     "insights.githubusercontent.com",
     "objects.githubusercontent.com",
@@ -47,7 +31,6 @@ const sites = [
     // Add other sites here
 ];
 
-let IPs = [];
 
 /**
  * @function getIP
@@ -56,15 +39,34 @@ let IPs = [];
  * @returns {Promise<string>} - 返回网站的 IP 地址
  */
 async function getIP(host) {
-    let url = `https://acme-check.com/?domain=${host}`;
+    let url = `http://ip-api.com/json/${host}?fields=status,message,query`;
     console.log(`${appName}: Getting IP for '${host}' ( ${url} )`);
     try {
-        const response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const ip = $("body > main > section.container-fluid.info-section > div > div > div:nth-child(1) > h4").text();
-        console.log(`${appName}: Got IP for '${host}' : ${ip}`);
-        return ip;
+        // 做到了0依赖，开心:)
+        const response = await fetch(url, {
+            method: 'GET',
+        });
+        if (response.ok) {
+            const data = await response.json();
+            if (data.status == 'success') {
+                const ip = data.query;
+                console.log(`${appName}: Got IP for '${host}' : ${ip}`);
+                return ip;
+            }
+            else {
+                console.log(`${appName}: ERROR - API returned an error message:\n${data.message}`);
+                return "";
+            }
+        }
+        else {
+            if (response.headers['X-R1'] <= 0) {
+                console.log(`${appName}: ERROR - The API call limit is reached`);
+                console.log(`${appName}: ERROR - Try again in at least ${response.headers['X-Ttl']} seconds`);
+                throw new Error('API call limit reached');
+            }
+            console.log(`${appName}: ERROR - returned an HTTP error code:\n${response.status} (${response.statusText})`);
+            return "";
+        }
     } catch (error) {
         console.log(`${appName}: ERROR - An error occurred while getting IP for '${host}' :`);
         console.error(error);
@@ -75,15 +77,105 @@ async function getIP(host) {
 /**
  * @function getIPs
  * 获取所有网站的 IP 地址
+ * @param {Boolean} - 是否缓存IP列表
  * @returns {Promise<Array>} - 返回包含所有网站 IP 地址的数组
  */
-async function getIPs() {
+async function getIPs(cache = true) {
     let promises = sites.map(async (site) => {
-        let ip = await getIP(site);
+        try {
+            let ip = await getIP(site);
         return { host: site, ip: ip };
+        }
+        catch (err) {
+            if (err.message == 'API call limit reached') {
+                exit(1);
+            }
+            else {
+                throw err;
+            }
+        }
     });
+    let IPs = await Promise.all(promises)
 
-    return Promise.all(promises);
+    if (cache) {
+        cacheIPs(IPs);
+    }
+    else {
+        console.info(`${appName}: will not cache IPs`);
+    }
+    return IPs;
 }
 
-module.exports = { getIPs };
+/**
+ * @function cacheIPs
+ * 缓存IP列表
+ * @param {Array} IPs - IP列表
+ */
+function cacheIPs(IPs) {
+    console.log(`${appName}: Caching IPs...`);
+    let now = Date.now();
+    let cache = {
+        time: now,
+        IPs: IPs,
+    };
+    let json = JSON.stringify(cache);
+    try {
+        if (!fs.existsSync(path.join(__dirname, "files/cache"))) {
+            fs.mkdirSync(path.join(__dirname, "files/cache"), {
+                recursive: true,
+            });
+        }
+        fs.writeFileSync(path.join(__dirname, "files/cache/cache.json"), json);
+        console.info(`${appName}: Successfully cached IPs`);
+    }
+    catch (error) {
+        console.error(`${appName}: ERROR - An error occurred while caching IPs :`);
+        console.error(error);
+    }
+}
+
+/**
+ * @function readCache
+ * 读取缓存
+ * @returns {Array} 缓存的IP列表 
+ */
+function readCache() {
+    try {
+        if (fs.existsSync(path.join(__dirname, "files/cache/cache.json"))) {
+            let data = fs.readFileSync(path.join(__dirname, "files/cache/cache.json"));
+            let cache = JSON.parse(data);
+            let now = Date.now();
+            if (now - cache.time < 60 * 60 * 3) { // 保留3小时内的缓存
+                let flag = true;
+                for (let record in cache.IPs) { // 验证缓存格式
+                    if (!(typeof record.host === "string" && typeof record.ip === "string")) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    return cache.IPs;
+                }
+                else {
+                    console.info(`${appName}: cache file corrupted, ignore it`);
+                    return null;
+                }
+            }
+            else {
+                console.info(`${appName}: cache file expired, ignore it`);
+                return null;
+            }
+        }
+        else {
+            console.info(`${appName}: cache file not exist, ignore it`);
+            return null;
+        }
+    }
+    catch (error) {
+        console.error(`${appName}: ERROR - An error occurred while reading cache:`);
+        console.error(error);
+        return null;
+    }
+}
+
+module.exports = { getIPs, readCache };
